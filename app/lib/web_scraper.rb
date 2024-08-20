6849b3b7c21b8499f031e733b9772e21df6f5f0f
樗木恵美子 <chishakiemiko@chishakiemiko.local>
2024-08-20 22:22:38 +0900

    スクレイパー追加

--- app/lib/web_scraper.rb
+++ app/lib/web_scraper.rb
@@ -1,10 +1,5 @@
 class WebScraper
-  def self.get_title(url)
-    agent = Mechanize.new
-    page = agent.get(url)
-    page.title
-  end
-
+  
 # キータの投稿者名のスクレイピングメソッド
   def self.get_qiita_post_contributors
     agent = Mechanize.new
@@ -16,7 +11,7 @@ class WebScraper
       puts contributor_texts
   end
 
-  # キータの投稿タイトルをスクレイピングするメソッド
+  # キータの投稿タイトルを5つスクレイピングするメソッド
   def self.get_qiita_post_titles
     agent = Mechanize.new
     page = agent.get('https://qiita.com')
@@ -25,7 +20,7 @@ class WebScraper
       post_titles_texts << post_title_text.inner_text
     end
     puts post_titles_texts.slice(0..4)
-      # puts post_titles_texts
+     
   end
 
   #  キータの投稿の上位５個のリンクを取得するメソッド
@@ -39,15 +34,11 @@ class WebScraper
       post_title_link = element.get_attribute('href')
       post_title_links << url + post_title_link if post_title_link.present?
     end
-    # puts post_title_links.slice(0..4)
-      # puts post_title_links
-
-    # post_title_links_5 = post_title_links[0..4]
-    post_title_links[0..4].each_with_index do |link, i|
-      puts " #{i+1}: #{link}"
+    
+    post_title_links[0..4].each do |link|
+      puts link
     end 
   end
-# end
 
 # キータlinksメソッドでページ内全てのリンクを配列として取得する
 
@@ -59,16 +50,6 @@ class WebScraper
   links = page.links
   links_href = links.map(&:href)
   puts links_href
-end
+ end
 
-
- #  キータの投稿の属性値とリンクを取得するメソッド
- def self.get_qiita_post_title_link_and_text
-  url = 'https://qiita.com'
-  agent = Mechanize.new
-  page = agent.get(url)
-  
-  links = page.links
-  links_href = links.map(&:href)
-  end
 end
